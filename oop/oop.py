bc767860f5da351cbb9bc4b977f6331c5cd23018
Ibrahim Zaky <dev-izaky@hotmail.com>
2025-12-07 16:50:51 +0200

    self

--- oop/oop.py
+++ oop/oop.py
@@ -1,10 +1,32 @@
 
 class computer:
-    def config(self):
-        print("i7, 16GB, 1TB")
+    def __init__(self,cpu,ram):
+        self.cpu = cpu
+        self.ram = ram
+        print("init function")
 
-com1 = computer()
+    def config(self):
+        print('Config: ',self.cpu,self.ram)
+
+    def update(self,cpu):
+        self.cpu = cpu
+        print('Update: ',self.cpu,self.ram)
+
+    def compare(self,other):
+        if self.ram == other.ram:
+            return True
+        else:
+            return False
+
+com1 = computer('AMD','8GB')
+com2 = computer('i9','64GB')
 
 #computer.config(com1)
 # Or
+com1.cpu = 'Ryzen5'
 com1.config()
+
+com1.update('Ryzen7')
+com2.config()
+
+print(com1.compare(com2))
